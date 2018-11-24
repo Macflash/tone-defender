b965f4dfe3c03fde3ccf87ed3d8df2a90655d85e
Alex Breyfogle <alexbr@microsoft.com>
2018-11-24 09:52:15 -0800

    Basic grid and resizing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+class Tile {
+
+}
 
 class App extends Component {
+  componentDidMount() {
+    this.canvas = document.getElementById('gameCanvas');
+    this.ctx = this.canvas.getContext('2d');
+    this.ctx.strokeStyle = 'grey';
+
+    window.addEventListener('resize', () => { this.reSize() }, false);
+
+    this.reSize();
+  }
+
+  reSize() {
+    if (!window || !this.canvas) { return; }
+    this.vpWidth = this.canvas.width = window.innerWidth - 10;
+    this.vpHeight = this.canvas.height = window.innerHeight - 10;
+    this.reDraw();
+  }
+
+  reDraw() {
+    if (!this.ctx) { return; }
+
+    this.ctx.clearRect(0, 0, this.vpWidth, this.vpHeight);
+
+    // number of tiles in the view
+    const tWidth = 8;
+    const tHeight = 8;
+    // resulting size of the tiles
+    const tileSize = Math.min(this.vpWidth / tWidth, this.vpHeight / tHeight);
+
+    for (var x = 0; x < tWidth; x++) {
+      for (var y = 0; y < tHeight; y++) {
+        this.ctx.strokeRect(x * tileSize, y * tileSize, tileSize, tileSize);
+      }
+    }
+
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Blagh
-          </a>
-        </header>
-      </div>
-    );
+    return <canvas id="gameCanvas" />;
   }
 }
 
